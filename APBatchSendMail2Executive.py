48bfb172c3b851c354e96a4e4ac33397fb1174d1
ossuchas <suchat.s14102526@gmail.com>
2020-01-17 13:05:33 +0700

    added#2

--- APBatchSendMail2Executive.py
+++ APBatchSendMail2Executive.py
@@ -1,9 +1,5 @@
 import logging
-from pathlib import Path
-import requests
-# import os.path
-from os import path, environ, remove
-import re
+from os import path
 import smtplib
 import urllib
 from email.mime.application import MIMEApplication
@@ -14,6 +10,7 @@ import pyodbc
 import pandas as pd
 from sqlalchemy import create_engine
 from config import MAIL_SENDER, MAIL_BODY, MAIL_SUBJECT
+import codecs
 
 
 class ConnectDB:
@@ -95,23 +92,185 @@ def send_email(subject, message, from_email, to_email=None, attachment=None):
     return
 
 
+def getHeaderTable(p_parm: int = None):
+    strSQL = """
+    EXEC [dbo].[sp_proc_mail_llbw_H] @p_parm = ?
+    """
+    params = (p_parm,)
+
+    myConnDB = ConnectDB()
+    result_set = myConnDB.exec_spRet(strSQL, params=params)
+    returnVal = []
+    for row in result_set:
+        # print(row.head_subject)
+        returnVal.append(row.head_subject)
+        returnVal.append(row.head_curr_quarter)
+        returnVal.append(row.head_curr_quarter_m1)
+        returnVal.append(row.head_curr_quarter_m2)
+        returnVal.append(row.head_curr_quarter_m3)
+        returnVal.append(row.head_curr_qtd_w)
+        returnVal.append(row.head_curr_month)
+        returnVal.append(row.head_curr_month_w1)
+        returnVal.append(row.head_curr_month_w2)
+        returnVal.append(row.head_curr_month_w3)
+        returnVal.append(row.head_curr_month_w4)
+        returnVal.append(row.head_curr_month_w5)
+
+    return returnVal
+
+
+def readhtml(p_parm: int = None):
+    f = None
+    if p_parm == 1:
+        f = codecs.open("templates/template_sec1_w5.html", 'r')
+    if p_parm == 2:
+        f = codecs.open("templates/template_sec2_w5.html", 'r')
+    if p_parm == 3:
+        f = codecs.open("templates/template_sec3_w5.html", 'r')
+    return f.read()
+
+
+def readhtmlw4(p_parm: int = None):
+    f = None
+    if p_parm == 1:
+        f = codecs.open("templates/template_sec1_w4.html", 'r')
+    if p_parm == 2:
+        f = codecs.open("templates/template_sec2_w4.html", 'r')
+    if p_parm == 3:
+        f = codecs.open("templates/template_sec3_w4.html", 'r')
+    return f.read()
+
+
 def main():
-    # GET transfer number list
-    params = 'Driver={ODBC Driver 17 for SQL Server};Server=192.168.2.58;Database=db_iconcrm_fusion;uid=iconuser;pwd' \
-             '=P@ssw0rd; '
-    params = urllib.parse.quote_plus(params)
-    db = create_engine('mssql+pyodbc:///?odbc_connect=%s' % params, fast_executemany=True)
+    # params = 'Driver={ODBC Driver 17 for SQL Server};Server=192.168.2.58;Database=db_iconcrm_fusion;uid=iconuser;pwd' \
+    #          '=P@ssw0rd; '
+    # params = urllib.parse.quote_plus(params)
+    # db = create_engine('mssql+pyodbc:///?odbc_connect=%s' % params, fast_executemany=True)
 
     receivers = ['suchat_s@apthai.com']
-    # receivers = emaillist
-    subject = "test"
-    bodyMsg = MAIL_BODY
+    subject = MAIL_SUBJECT
+    # bodyMsg = MAIL_BODY
+    bodyMsg = generateHTML()
     sender = MAIL_SENDER
 
     attachedFile = []
 
     # Send Email to Customer
     send_email(subject, bodyMsg, sender, receivers, attachedFile)
+
+
+def generateHTML():
+
+    strSQL = """
+        SELECT COUNT(*) AS total_record
+    	FROM dbo.BI_Calendar_Week WITH(NOLOCK)
+    	WHERE 1=1
+    	AND DATEPART(YEAR, StartDate) = DATEPART(YEAR,GETDATE())
+    	AND DATEPART(QUARTER, StartDate) = DATEPART(QUARTER,GETDATE())
+    	AND DATEPART(MONTH, StartDate) = DATEPART(MONTH,GETDATE())
+        """
+
+    myConnDB = ConnectDB()
+    result_set = myConnDB.query(strSQL)
+    returnVal = []
+    for row in result_set:
+        returnVal.append(row.total_record)
+
+    total_week = returnVal[0]
+    if total_week == 5:
+        return readhtml(1) \
+               .format(getHeaderTable(0)[0], getHeaderTable(0)[1], getHeaderTable(0)[2],
+                       getHeaderTable(0)[3], getHeaderTable(0)[4], getHeaderTable(0)[5],
+                       getHeaderTable(0)[6], getHeaderTable(0)[7], getHeaderTable(0)[8],
+                       getHeaderTable(0)[9], getHeaderTable(0)[10], getHeaderTable(0)[11]) \
+           + "<br />" + readhtml(1) \
+               .format(getHeaderTable(1)[0], getHeaderTable(1)[1], getHeaderTable(1)[2],
+                       getHeaderTable(1)[3], getHeaderTable(1)[4], getHeaderTable(1)[5],
+                       getHeaderTable(1)[6], getHeaderTable(1)[7], getHeaderTable(1)[8],
+                       getHeaderTable(1)[9], getHeaderTable(1)[10], getHeaderTable(1)[11]) \
+           + "<br />" + readhtml(1) \
+               .format(getHeaderTable(2)[0], getHeaderTable(2)[1], getHeaderTable(2)[2],
+                       getHeaderTable(2)[3], getHeaderTable(2)[4], getHeaderTable(2)[5],
+                       getHeaderTable(2)[6], getHeaderTable(2)[7], getHeaderTable(2)[8],
+                       getHeaderTable(2)[9], getHeaderTable(2)[10], getHeaderTable(2)[11]) \
+           + "<br />" + readhtml(1) \
+               .format(getHeaderTable(3)[0], getHeaderTable(3)[1], getHeaderTable(3)[2],
+                       getHeaderTable(3)[3], getHeaderTable(3)[4], getHeaderTable(3)[5],
+                       getHeaderTable(3)[6], getHeaderTable(3)[7], getHeaderTable(3)[8],
+                       getHeaderTable(3)[9], getHeaderTable(3)[10], getHeaderTable(3)[11]) \
+           + "<br />" + readhtml(1) \
+               .format(getHeaderTable(4)[0], getHeaderTable(4)[1], getHeaderTable(4)[2],
+                       getHeaderTable(4)[3], getHeaderTable(4)[4], getHeaderTable(4)[5],
+                       getHeaderTable(4)[6], getHeaderTable(4)[7], getHeaderTable(4)[8],
+                       getHeaderTable(4)[9], getHeaderTable(4)[10], getHeaderTable(4)[11]) \
+           + "<br />" + readhtml(1) \
+               .format(getHeaderTable(5)[0], getHeaderTable(5)[1], getHeaderTable(5)[2],
+                       getHeaderTable(5)[3], getHeaderTable(5)[4], getHeaderTable(5)[5],
+                       getHeaderTable(5)[6], getHeaderTable(5)[7], getHeaderTable(5)[8],
+                       getHeaderTable(5)[9], getHeaderTable(5)[10], getHeaderTable(5)[11]) \
+           + "<br />" + readhtml(1) \
+               .format(getHeaderTable(6)[0], getHeaderTable(6)[1], getHeaderTable(6)[2],
+                       getHeaderTable(6)[3], getHeaderTable(6)[4], getHeaderTable(6)[5],
+                       getHeaderTable(6)[6], getHeaderTable(6)[7], getHeaderTable(6)[8],
+                       getHeaderTable(6)[9], getHeaderTable(6)[10], getHeaderTable(6)[11]) \
+           + "<br />" + readhtml(2) \
+               .format(getHeaderTable(3)[0], getHeaderTable(3)[1], getHeaderTable(3)[2],
+                       getHeaderTable(3)[3], getHeaderTable(3)[4], getHeaderTable(3)[5],
+                       getHeaderTable(3)[6], getHeaderTable(3)[7], getHeaderTable(3)[8],
+                       getHeaderTable(3)[9], getHeaderTable(3)[10], getHeaderTable(3)[11]) \
+           + "<br />" + readhtml(3) \
+               .format(getHeaderTable(7)[0], getHeaderTable(7)[1], getHeaderTable(7)[2],
+                       getHeaderTable(7)[3], getHeaderTable(7)[4], getHeaderTable(7)[5],
+                       getHeaderTable(7)[6], getHeaderTable(7)[7], getHeaderTable(7)[8],
+                       getHeaderTable(7)[9], getHeaderTable(7)[10], getHeaderTable(7)[11]) \
+           + "<br />"
+    else:
+        return readhtmlw4(1) \
+                   .format(getHeaderTable(0)[0], getHeaderTable(0)[1], getHeaderTable(0)[2],
+                           getHeaderTable(0)[3], getHeaderTable(0)[4], getHeaderTable(0)[5],
+                           getHeaderTable(0)[6], getHeaderTable(0)[7], getHeaderTable(0)[8],
+                           getHeaderTable(0)[9], getHeaderTable(0)[10], getHeaderTable(0)[11]) \
+               + "<br />" + readhtmlw4(1) \
+                   .format(getHeaderTable(1)[0], getHeaderTable(1)[1], getHeaderTable(1)[2],
+                           getHeaderTable(1)[3], getHeaderTable(1)[4], getHeaderTable(1)[5],
+                           getHeaderTable(1)[6], getHeaderTable(1)[7], getHeaderTable(1)[8],
+                           getHeaderTable(1)[9], getHeaderTable(1)[10], getHeaderTable(1)[11]) \
+               + "<br />" + readhtmlw4(1) \
+                   .format(getHeaderTable(2)[0], getHeaderTable(2)[1], getHeaderTable(2)[2],
+                           getHeaderTable(2)[3], getHeaderTable(2)[4], getHeaderTable(2)[5],
+                           getHeaderTable(2)[6], getHeaderTable(2)[7], getHeaderTable(2)[8],
+                           getHeaderTable(2)[9], getHeaderTable(2)[10], getHeaderTable(2)[11]) \
+               + "<br />" + readhtmlw4(1) \
+                   .format(getHeaderTable(3)[0], getHeaderTable(3)[1], getHeaderTable(3)[2],
+                           getHeaderTable(3)[3], getHeaderTable(3)[4], getHeaderTable(3)[5],
+                           getHeaderTable(3)[6], getHeaderTable(3)[7], getHeaderTable(3)[8],
+                           getHeaderTable(3)[9], getHeaderTable(3)[10], getHeaderTable(3)[11]) \
+               + "<br />" + readhtmlw4(1) \
+                   .format(getHeaderTable(4)[0], getHeaderTable(4)[1], getHeaderTable(4)[2],
+                           getHeaderTable(4)[3], getHeaderTable(4)[4], getHeaderTable(4)[5],
+                           getHeaderTable(4)[6], getHeaderTable(4)[7], getHeaderTable(4)[8],
+                           getHeaderTable(4)[9], getHeaderTable(4)[10], getHeaderTable(4)[11]) \
+               + "<br />" + readhtmlw4(1) \
+                   .format(getHeaderTable(5)[0], getHeaderTable(5)[1], getHeaderTable(5)[2],
+                           getHeaderTable(5)[3], getHeaderTable(5)[4], getHeaderTable(5)[5],
+                           getHeaderTable(5)[6], getHeaderTable(5)[7], getHeaderTable(5)[8],
+                           getHeaderTable(5)[9], getHeaderTable(5)[10], getHeaderTable(5)[11]) \
+               + "<br />" + readhtmlw4(1) \
+                   .format(getHeaderTable(6)[0], getHeaderTable(6)[1], getHeaderTable(6)[2],
+                           getHeaderTable(6)[3], getHeaderTable(6)[4], getHeaderTable(6)[5],
+                           getHeaderTable(6)[6], getHeaderTable(6)[7], getHeaderTable(6)[8],
+                           getHeaderTable(6)[9], getHeaderTable(6)[10], getHeaderTable(6)[11]) \
+               + "<br />" + readhtmlw4(2) \
+                   .format(getHeaderTable(3)[0], getHeaderTable(3)[1], getHeaderTable(3)[2],
+                           getHeaderTable(3)[3], getHeaderTable(3)[4], getHeaderTable(3)[5],
+                           getHeaderTable(3)[6], getHeaderTable(3)[7], getHeaderTable(3)[8],
+                           getHeaderTable(3)[9], getHeaderTable(3)[10], getHeaderTable(3)[11]) \
+               + "<br />" + readhtmlw4(3) \
+                   .format(getHeaderTable(7)[0], getHeaderTable(7)[1], getHeaderTable(7)[2],
+                           getHeaderTable(7)[3], getHeaderTable(7)[4], getHeaderTable(7)[5],
+                           getHeaderTable(7)[6], getHeaderTable(7)[7], getHeaderTable(7)[8],
+                           getHeaderTable(7)[9], getHeaderTable(7)[10], getHeaderTable(7)[11]) \
+               + "<br />"
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,15 +1,189 @@
 import os
 # Mail Setting
 MAIL_SENDER = "noreply@apthai.com"
-MAIL_SUBJECT = "หนังสือประกอบการยื่นยกเว้นภาษี กรณีบ้านหลังแรก"
-MAIL_BODY = """<p style="font-family:AP;">เรียนลูกค้า<br>&nbsp;&nbsp;&nbsp;&nbsp;\
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;กลุ่มบริษัทเอพี ขอจัดส่งหนังสือรับรองจำนวนเงินที่ชำระค่าซื้ออสังหาริมทรัพย์ \
-    เพื่อใช้ประกอบการยื่นยกเว้นภาษี กรณีบ้านหลังแรก \
-    <br />ลูกค้าสามารถดาวน์โหลดแบบฟอร์มได้ตามไฟส์แนบ \
-    <br /><p style="font-family:AP;"><a href="http://www.rd.go.th/fileadmin/user_upload/kormor/newlaw/dg353.pdf">\
-    ประกาศจากกรมสรรพากร</a></p><br /><br  /> \
-    ขอแสดงความนับถือ<br /><img src="http://www.apintranet.com/static/media/logo.d9fe4116.png"\
-    alt="ap" width="5%" ><br />บมจ.เอพี (ไทยแลนด์) และบริษัทในเครือ<br /></p>"""
+MAIL_SUBJECT = "Weekly Summary Lead Lag"
+MAIL_BODY = """
+<body>
+<table border="1" cellpadding="0" cellspacing="0" style="font-family:Calibri;font-size: 9pt;">
+  <col width="166" />
+  <col width="61" span="3" />
+  <col width="70" />
+  <tr>
+    <td align="right" width="166">&nbsp;</td>
+    <td colspan="4" align="center">Q4</td>
+    <td colspan="3" align="center">QTD</td>
+    <td width="70" rowspan="2" align="center">QTG</td>
+    <td colspan="8" align="center">Dec</td>
+  </tr>
+  <tr>
+    <td rowspan="2">Pre-sales</td>
+    <td width="61" align="center">Oct</td>
+    <td width="61" align="center">Nov</td>
+    <td width="61" align="center">Dec</td>
+    <td width="70" rowspan="2" align="center">Total </td>
+    <td width="70" align="center">Target</td>
+    <td width="70" align="center">Actual</td>
+    <td width="70" align="center">Var</td>
+    <td width="70" align="center">&nbsp;</td>
+    <td width="70" align="center">&nbsp;</td>
+    <td width="70" align="center">&nbsp;</td>
+    <td width="70" align="center">&nbsp;</td>
+    <td width="70" align="center">&nbsp;</td>
+    <td colspan="3" align="center">Full Month</td>
+  </tr>
+  <tr>
+    <td width="61" align="center"> Target </td>
+    <td width="61" align="center"> Target </td>
+    <td width="61" align="center"> Target </td>
+    <td width="70" align="center">&nbsp;</td>
+    <td width="70" align="center">&nbsp;</td>
+    <td width="70" align="center">&nbsp;</td>
+    <td width="70" align="center">&nbsp;</td>
+    <td width="70" align="center">Actual</td>
+    <td width="70" align="center">Actual</td>
+    <td width="70" align="center">Actual</td>
+    <td width="70" align="center">Target</td>
+    <td width="70" align="center">Actual</td>
+    <td width="70" align="center">Target</td>
+    <td width="70" align="center">Actual</td>
+    <td width="70" align="center">Var</td>
+  </tr>
+  <tr>
+    <td align="left">BG-SDH </td>
+    <td align="right">854</td>
+    <td align="right">718</td>
+    <td align="right">525</td>
+    <td align="right">2,098</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="left">BG-TH</td>
+    <td align="right">1,264</td>
+    <td align="right">1,336</td>
+    <td align="right">742</td>
+    <td align="right">3,342</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="left">TH1 - BKM</td>
+    <td align="right">560</td>
+    <td align="right">631</td>
+    <td align="right">387</td>
+    <td align="right">1,577</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="left">TH2 - Pleno</td>
+    <td align="right">704</td>
+    <td align="right">705</td>
+    <td align="right">355</td>
+    <td align="right">1,765</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="left">BG-CD1 (AP+JV100%) </td>
+    <td align="right">436</td>
+    <td align="right">1,237</td>
+    <td align="right">552</td>
+    <td align="right">2,224</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="left">BG-CD2 (AP+JV100%)</td>
+    <td align="right">472</td>
+    <td align="right">452</td>
+    <td align="right">301</td>
+    <td align="right">1,225</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="left">Total</td>
+    <td align="right">3,026</td>
+    <td align="right">3,743</td>
+    <td align="right">2,120</td>
+    <td align="right">8,888</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+    <td align="right">&nbsp;</td>
+  </tr>
+</table>
+</body>
+    """
 
 
 
