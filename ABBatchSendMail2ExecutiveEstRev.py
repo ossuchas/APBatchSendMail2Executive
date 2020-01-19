7caafead89898e208657762e1a508ffbdc43afa5
ossuchas <suchat.s14102526@gmail.com>
2020-01-19 18:45:17 +0700

    add new file estimate revenue

---
+++ ABBatchSendMail2ExecutiveEstRev.py
@@ -0,0 +1,140 @@
+import logging
+from os import path
+import smtplib
+import urllib
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import pyodbc
+import pandas as pd
+from sqlalchemy import create_engine
+from config import MAIL_SENDER,  EST_MAIL_SUBJECT, EST_MAIL_TABLE_HEAD, EST_MAIL_TABLE_COL_HEAD
+import codecs
+
+
+class ConnectDB:
+    def __init__(self):
+        ''' Constructor for this class. '''
+        self._connection = pyodbc.connect(
+            'Driver={ODBC Driver 17 for SQL Server};Server=192.168.2.58;Database=db_iconcrm_fusion;uid=iconuser;pwd=P@ssw0rd;')
+        self._cursor = self._connection.cursor()
+
+    def query(self, query):
+        try:
+            result = self._cursor.execute(query)
+        except Exception as e:
+            logging.error('error execting query "{}", error: {}'.format(query, e))
+            return None
+        finally:
+            return result
+
+    def update(self, sqlStatement):
+        try:
+            self._cursor.execute(sqlStatement)
+        except Exception as e:
+            logging.error('error execting Statement "{}", error: {}'.format(sqlStatement, e))
+            return None
+        finally:
+            self._cursor.commit()
+
+    def exec_sp(self, sqlStatement, params):
+        try:
+            self._cursor.execute(sqlStatement, params)
+        except Exception as e:
+            logging.error('error execting Statement "{}", error: {}'.format(sqlStatement, e))
+            return None
+        finally:
+            self._cursor.commit()
+
+    def exec_spRet(self, sqlStatement, params):
+        try:
+            result = self._cursor.execute(sqlStatement, params)
+        except Exception as e:
+            print('error execting Statement "{}", error: {}'.format(sqlStatement, e))
+            return None
+        finally:
+            return result
+
+    def __del__(self):
+        self._cursor.close()
+
+
+def send_email(subject, message, from_email, to_email=None, attachment=None):
+    """
+    :param subject: email subject
+    :param message: Body content of the email (string), can be HTML/CSS or plain text
+    :param from_email: Email address from where the email is sent
+    :param to_email: List of email recipients, example: ["a@a.com", "b@b.com"]
+    :param attachment: List of attachments, exmaple: ["file1.txt", "file2.txt"]
+    """
+    if attachment is None:
+        attachment = []
+    if to_email is None:
+        to_email = []
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = ", ".join(to_email)
+    msg.attach(MIMEText(message, 'html'))
+
+    for f in attachment:
+        with open(f, 'rb') as a_file:
+            basename = path.basename(f)
+            part = MIMEApplication(a_file.read(), Name=basename)
+
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename
+        msg.attach(part)
+
+    email = smtplib.SMTP('apmail.apthai.com', 25)
+    email.sendmail(from_email, to_email, msg.as_string())
+    email.quit()
+    return
+
+
+def generateHTML():
+    strSQL = """
+    SELECT subject_name, qtd_curr_w_ac, qtd_curr_w_diff, qtg_val, curr_m_w1_ac 
+	FROM dbo.crm_mail_ll_data WHERE subject_no = 1 ORDER BY tran_id
+    """
+
+    myConnDB = ConnectDB()
+    result_set = myConnDB.query(strSQL)
+
+    strHTML = ""
+    str_tmpHTML = """
+    <tr>
+    <td align="left"><strong>{0}</strong></td>
+    <td align="right" >{1}&nbsp;</td>
+    <td align="right">{2}&nbsp;</td>
+    <td align="right">{3}&nbsp;</td>
+    <td align="right">{4}&nbsp;</td>
+    </tr>
+    """
+
+    for row in result_set:
+        # print(row.subject_name.strip(), row.qtd_curr_w_ac, row.qtd_curr_w_diff, row.qtg_val, row.curr_m_w1_ac )
+        strHTML = strHTML.strip() + str_tmpHTML.format(row.subject_name.strip(), f"{row.qtd_curr_w_ac:,.0f}",
+                                                       f"{row.qtd_curr_w_diff:,.0f}",
+                                                       f"{row.qtg_val:,.0f}", f"{row.curr_m_w1_ac:,.0f}")
+
+    return """
+    <body>{0}{1}{2}</body>
+    """.format(EST_MAIL_TABLE_HEAD, EST_MAIL_TABLE_COL_HEAD, strHTML)
+
+
+def main():
+    # receivers = ['suchat_s@apthai.com', 'apichaya@apthai.com', 'jintana_i@apthai.com', 'polwaritpakorn@apthai.com']
+    receivers = ['suchat_s@apthai.com']
+    subject = EST_MAIL_SUBJECT
+    bodyMsg = generateHTML()
+    sender = MAIL_SENDER
+
+    attachedFile = []
+
+    # Send Email to Customer
+    send_email(subject, bodyMsg, sender, receivers, attachedFile)
+
+
+if __name__ == '__main__':
+    main()
--- config.py
+++ config.py
@@ -1,189 +1,34 @@
-import os
 # Mail Setting
 MAIL_SENDER = "noreply@apthai.com"
 MAIL_SUBJECT = "Weekly Summary Lead Lag"
 MAIL_BODY = """
-<body>
-<table border="1" cellpadding="0" cellspacing="0" style="font-family:Calibri;font-size: 9pt;">
-  <col width="166" />
-  <col width="61" span="3" />
-  <col width="70" />
-  <tr>
-    <td align="right" width="166">&nbsp;</td>
-    <td colspan="4" align="center">Q4</td>
-    <td colspan="3" align="center">QTD</td>
-    <td width="70" rowspan="2" align="center">QTG</td>
-    <td colspan="8" align="center">Dec</td>
-  </tr>
-  <tr>
-    <td rowspan="2">Pre-sales</td>
-    <td width="61" align="center">Oct</td>
-    <td width="61" align="center">Nov</td>
-    <td width="61" align="center">Dec</td>
-    <td width="70" rowspan="2" align="center">Total </td>
-    <td width="70" align="center">Target</td>
-    <td width="70" align="center">Actual</td>
-    <td width="70" align="center">Var</td>
-    <td width="70" align="center">&nbsp;</td>
-    <td width="70" align="center">&nbsp;</td>
-    <td width="70" align="center">&nbsp;</td>
-    <td width="70" align="center">&nbsp;</td>
-    <td width="70" align="center">&nbsp;</td>
-    <td colspan="3" align="center">Full Month</td>
-  </tr>
-  <tr>
-    <td width="61" align="center"> Target </td>
-    <td width="61" align="center"> Target </td>
-    <td width="61" align="center"> Target </td>
-    <td width="70" align="center">&nbsp;</td>
-    <td width="70" align="center">&nbsp;</td>
-    <td width="70" align="center">&nbsp;</td>
-    <td width="70" align="center">&nbsp;</td>
-    <td width="70" align="center">Actual</td>
-    <td width="70" align="center">Actual</td>
-    <td width="70" align="center">Actual</td>
-    <td width="70" align="center">Target</td>
-    <td width="70" align="center">Actual</td>
-    <td width="70" align="center">Target</td>
-    <td width="70" align="center">Actual</td>
-    <td width="70" align="center">Var</td>
-  </tr>
-  <tr>
-    <td align="left">BG-SDH </td>
-    <td align="right">854</td>
-    <td align="right">718</td>
-    <td align="right">525</td>
-    <td align="right">2,098</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="left">BG-TH</td>
-    <td align="right">1,264</td>
-    <td align="right">1,336</td>
-    <td align="right">742</td>
-    <td align="right">3,342</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="left">TH1 - BKM</td>
-    <td align="right">560</td>
-    <td align="right">631</td>
-    <td align="right">387</td>
-    <td align="right">1,577</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="left">TH2 - Pleno</td>
-    <td align="right">704</td>
-    <td align="right">705</td>
-    <td align="right">355</td>
-    <td align="right">1,765</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="left">BG-CD1 (AP+JV100%) </td>
-    <td align="right">436</td>
-    <td align="right">1,237</td>
-    <td align="right">552</td>
-    <td align="right">2,224</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="left">BG-CD2 (AP+JV100%)</td>
-    <td align="right">472</td>
-    <td align="right">452</td>
-    <td align="right">301</td>
-    <td align="right">1,225</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="left">Total</td>
-    <td align="right">3,026</td>
-    <td align="right">3,743</td>
-    <td align="right">2,120</td>
-    <td align="right">8,888</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-    <td align="right">&nbsp;</td>
-  </tr>
-</table>
-</body>
     """
 
 
-
+# Mail Settting Estimate Revenue
+EST_MAIL_SUBJECT = "Weekly Summary Estimate Revenue"
+EST_MAIL_BODY = """
+    """
+EST_MAIL_TABLE_HEAD = """
+<table cellpadding="0" cellspacing="0" border="1" style="font-family:AP;font-size: 8.5pt;border: 1px solid black;border-collapse: collapse;">
+  <col width="166" />
+  <col width="61" span="3" />
+  <col width="70" />
+  """
+EST_MAIL_TABLE_COL_HEAD = """
+<tr>
+    <td width="166" rowspan="3" align="center" bgcolor="#CCCCCC"><strong>Transfer</strong></td>
+    <td colspan="4" align="center" bgcolor="#CCCCCC"><strong>Q1</strong></td>
+  </tr>
+  <tr>
+    <td width="61" align="center" bgcolor="#CCCCCC"><strong>Jan</strong></td>
+    <td width="61" align="center" bgcolor="#CCCCCC"><strong>Feb</strong></td>
+    <td width="61" align="center" bgcolor="#CCCCCC"><strong>Mar</strong></td>
+    <td width="70" rowspan="2" align="right" bgcolor="#CCCCCC"><strong>Total Q1</strong></td>
+  </tr>
+  <tr>
+    <td width="61" align="center" bgcolor="#CCCCCC"><strong>Target</strong></td>
+    <td width="61" align="center" bgcolor="#CCCCCC"><strong>Target</strong></td>
+    <td width="61" align="center" bgcolor="#CCCCCC"><strong>Target</strong></td>
+  </tr>
+"""
