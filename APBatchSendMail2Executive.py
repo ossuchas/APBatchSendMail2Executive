2686c30b78882b05eb4784bb3dfda5685ba5ad7e
ossuchas <suchat.s14102526@gmail.com>
2020-01-23 10:00:30 +0700

    added

--- APBatchSendMail2Executive.py
+++ APBatchSendMail2Executive.py
@@ -171,8 +171,12 @@ def main():
     # Refresh Data
     refreshDataLastUpdate()
 
+    # receivers = ['suchat_s@apthai.com', 'apichaya@apthai.com', 'jintana_i@apthai.com', 'polwaritpakorn@apthai.com',
+    #              'tanonchai@apthai.com', 'teerapat_s@apthai.com']
+
     receivers = ['suchat_s@apthai.com', 'apichaya@apthai.com', 'jintana_i@apthai.com', 'polwaritpakorn@apthai.com',
-                 'tanonchai@apthai.com', 'teerapat_s@apthai.com']
+                 'tanonchai@apthai.com', 'teerapat_s@apthai.com', 'laddawan_v@apthai.com', 'woraphan_c@apthai.com',
+                 'raweewan_p@apthai.com', 'srisakul_p@apthai.com', 'jatuporn_p@apthai.com']
     # receivers = ['suchat_s@apthai.com']
     subject = MAIL_SUBJECT
     # bodyMsg = MAIL_BODY
