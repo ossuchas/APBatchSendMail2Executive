f59c138297d7fbaf3ef24fc808e44be4165d315a
ossuchas <suchat.s14102526@gmail.com>
2020-01-27 05:29:11 +0700

    modified estimate revenue

--- ABBatchSendMail2ExecutiveEstRev.py
+++ ABBatchSendMail2ExecutiveEstRev.py
@@ -92,10 +92,12 @@ def send_email(subject, message, from_email, to_email=None, attachment=None):
     return
 
 
-def generateHTML():
+def generateHTMLDetl():
     strSQL = """
-    SELECT subject_name, qtd_curr_w_ac, qtd_curr_w_diff, qtg_val, curr_m_w1_ac 
-	FROM dbo.crm_mail_ll_data WHERE subject_no = 1 ORDER BY tran_id
+    SELECT seqn_no, trns_name, ac_q1, ac_q2, ac_q3, ac_q4, qtd_curr_ac, est_curr_d1, est_curr_d2, est_curr_d3,
+    est_curr_d4, est_curr_d5, qtd_est_total, ytd_est_ac
+    FROM dbo.crm_mail_est_reve
+    ORDER BY seqn_no
     """
 
     myConnDB = ConnectDB()
@@ -104,23 +106,194 @@ def generateHTML():
     strHTML = ""
     str_tmpHTML = """
     <tr>
-    <td align="left"><strong>{0}</strong></td>
-    <td align="right" >{1}&nbsp;</td>
-    <td align="right">{2}&nbsp;</td>
-    <td align="right">{3}&nbsp;</td>
-    <td align="right">{4}&nbsp;</td>
+        <td align="left">{0}</td>
+        <td align="right">{1}&nbsp;</td>
+        <td align="right">{2}&nbsp;</td>
+        <td align="right">{3}&nbsp;</td>
+        <td align="right">{4}&nbsp;</td>
+        <td align="right">{5}&nbsp;</td>
+        <td align="right">{6}&nbsp;</td>
+        <td align="right">{7}&nbsp;</td>
+        <td align="right">{8}&nbsp;</td>
+        <td align="right">{9}&nbsp;</td>
+        <td align="right">{10}&nbsp;</td>
+        <td align="right">{11}&nbsp;</td>
+        <td align="right">{12}&nbsp;</td>
+    </tr>
+    """
+
+    str_total_tmpHTML = """
+    <tr>
+        <td align="right" bgcolor="#FFFFCC"><strong>{0}</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{1}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{2}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{3}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{4}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{5}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{6}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{7}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{8}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{9}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{10}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{11}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{12}&nbsp;</strong></td>
     </tr>
     """
 
     for row in result_set:
-        # print(row.subject_name.strip(), row.qtd_curr_w_ac, row.qtd_curr_w_diff, row.qtg_val, row.curr_m_w1_ac )
-        strHTML = strHTML.strip() + str_tmpHTML.format(row.subject_name.strip(), f"{row.qtd_curr_w_ac:,.0f}",
-                                                       f"{row.qtd_curr_w_diff:,.0f}",
-                                                       f"{row.qtg_val:,.0f}", f"{row.curr_m_w1_ac:,.0f}")
+        if row.seqn_no != 3:
+            strHTML = strHTML.strip() + str_tmpHTML.format(row.trns_name.strip(),
+                                                           f"{row.ac_q1:,.0f}",
+                                                           f"{row.ac_q2:,.0f}",
+                                                           f"{row.ac_q3:,.0f}",
+                                                           f"{row.ac_q4:,.0f}",
+                                                           f"{row.qtd_curr_ac:,.0f}",
+                                                           f"{row.est_curr_d1:,.0f}",
+                                                           f"{row.est_curr_d2:,.0f}",
+                                                           f"{row.est_curr_d3:,.0f}",
+                                                           f"{row.est_curr_d4:,.0f}",
+                                                           f"{row.est_curr_d5:,.0f}",
+                                                           f"{row.qtd_est_total:,.0f}",
+                                                           f"{row.ytd_est_ac:,.0f}"
+                                                           )
+        else:
+            strHTML = strHTML.strip() + str_total_tmpHTML.format(row.trns_name.strip(),
+                                                                 f"{row.ac_q1:,.0f}",
+                                                                 f"{row.ac_q2:,.0f}",
+                                                                 f"{row.ac_q3:,.0f}",
+                                                                 f"{row.ac_q4:,.0f}",
+                                                                 f"{row.qtd_curr_ac:,.0f}",
+                                                                 f"{row.est_curr_d1:,.0f}",
+                                                                 f"{row.est_curr_d2:,.0f}",
+                                                                 f"{row.est_curr_d3:,.0f}",
+                                                                 f"{row.est_curr_d4:,.0f}",
+                                                                 f"{row.est_curr_d5:,.0f}",
+                                                                 f"{row.qtd_est_total:,.0f}",
+                                                                 f"{row.ytd_est_ac:,.0f}"
+                                                                 )
 
-    return """
-    <body>{0}{1}{2}</body>
-    """.format(EST_MAIL_TABLE_HEAD, EST_MAIL_TABLE_COL_HEAD, strHTML)
+    return readHTMLFile(1).format(strHTML)
+
+
+def generateHTMLTotal():
+    strSQL = """
+    SELECT seqn_no, trns_name, ll_curr_q_tg, qtd_ac, est_curr_totl, qtd_est_totl, qtd_est_diff_ll_curr_q_tg
+    FROM dbo.crm_mail_est_reve_totl
+    ORDER BY seqn_no
+    """
+
+    myConnDB = ConnectDB()
+    result_set = myConnDB.query(strSQL)
+
+    strHTML = ""
+    str_tmpHTML = """
+    <tr>
+        <td align="left">{0}</td>
+        <td align="right">{1}&nbsp;</td>
+        <td align="right">{2}&nbsp;</td>
+        <td align="right">{3}&nbsp;</td>
+        <td align="right">{4}&nbsp;</td>
+        <td align="right">{5}&nbsp;</td>
+    </tr>
+    """
+
+    str_total_tmpHTML = """
+    <tr>
+        <td align="right" bgcolor="#FFFFCC"><strong>{0}</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{1}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{2}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{3}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{4}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{5}&nbsp;</strong></td>
+    </tr>
+    """
+
+    for row in result_set:
+        if row.seqn_no != 7:
+            strHTML = strHTML.strip() + str_tmpHTML.format(row.trns_name.strip(),
+                                                           f"{row.ll_curr_q_tg:,.0f}",
+                                                           f"{row.qtd_ac:,.0f}",
+                                                           f"{row.est_curr_totl:,.0f}",
+                                                           f"{row.qtd_est_totl:,.0f}",
+                                                           f"{row.qtd_est_diff_ll_curr_q_tg:,.0f}"
+                                                           )
+        else:
+            strHTML = strHTML.strip() + str_total_tmpHTML.format(row.trns_name.strip(),
+                                                           f"{row.ll_curr_q_tg:,.0f}",
+                                                           f"{row.qtd_ac:,.0f}",
+                                                           f"{row.est_curr_totl:,.0f}",
+                                                           f"{row.qtd_est_totl:,.0f}",
+                                                           f"{row.qtd_est_diff_ll_curr_q_tg:,.0f}"
+                                                           )
+
+    return readHTMLFile(2).format(strHTML)
+
+
+def generateHTMLbyProj():
+    strSQL = """
+    SELECT seqn_no, project_name, curr_q_m1_ac_u, curr_q_m1_ac_vol, curr_q_m2_ac_u, curr_q_m2_ac_vol, curr_q_m3_ac_u,
+    curr_q_m3_ac_vol, qtd_curr_ac_u, qtd_curr_q_ac_vol
+    FROM dbo.crm_mail_est_reve_byproj
+    ORDER BY seqn_no
+    """
+
+    myConnDB = ConnectDB()
+    result_set = myConnDB.query(strSQL)
+
+    strHTML = ""
+    str_tmpHTML = """
+    <tr>
+        <td align="left">{0}</td>
+        <td align="right" >{1}&nbsp;</td>
+        <td align="right" >{2}&nbsp;</td>
+        <td align="right">{3}&nbsp;</td>
+        <td align="right">{4}&nbsp;</td>
+        <td align="right">{5}&nbsp;</td>
+        <td align="right">{6}&nbsp;</td>
+        <td align="right">{7}&nbsp;</td>
+        <td align="right">{8}&nbsp;</td>
+    </tr>
+    """
+
+    str_total_tmpHTML = """
+    <tr>
+        <td align="right" bgcolor="#FFFFCC"><strong>{0}</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{1}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{2}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{3}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{4}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{5}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{6}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{7}&nbsp;</strong></td>
+        <td align="right" bgcolor="#FFFFCC"><strong>{8}&nbsp;</strong></td>
+    </tr>
+    """
+
+    for row in result_set:
+        if row.seqn_no != 6:
+            strHTML = strHTML.strip() + str_tmpHTML.format(row.project_name.strip(),
+                                                           f"{row.curr_q_m1_ac_u:,.0f}",
+                                                           f"{row.curr_q_m1_ac_vol:,.0f}",
+                                                           f"{row.curr_q_m2_ac_u:,.0f}",
+                                                           f"{row.curr_q_m2_ac_vol:,.0f}",
+                                                           f"{row.curr_q_m3_ac_u:,.0f}",
+                                                           f"{row.curr_q_m3_ac_vol:,.0f}",
+                                                           f"{row.qtd_curr_ac_u:,.0f}",
+                                                           f"{row.qtd_curr_q_ac_vol:,.0f}"
+                                                           )
+        else:
+            strHTML = strHTML.strip() + str_total_tmpHTML.format(row.project_name.strip(),
+                                                           f"{row.curr_q_m1_ac_u:,.0f}",
+                                                           f"{row.curr_q_m1_ac_vol:,.0f}",
+                                                           f"{row.curr_q_m2_ac_u:,.0f}",
+                                                           f"{row.curr_q_m2_ac_vol:,.0f}",
+                                                           f"{row.curr_q_m3_ac_u:,.0f}",
+                                                           f"{row.curr_q_m3_ac_vol:,.0f}",
+                                                           f"{row.qtd_curr_ac_u:,.0f}",
+                                                           f"{row.qtd_curr_q_ac_vol:,.0f}"
+                                                           )
+
+    return readHTMLFile(3).format(strHTML)
 
 
 def readHTMLFile(p_parm: int = None):
@@ -128,18 +301,20 @@ def readHTMLFile(p_parm: int = None):
     if p_parm == 1:
         f = codecs.open("templates/template_est_rev.html", 'r')
     if p_parm == 2:
+        f = codecs.open("templates/template_est_rev_total.html", 'r')
+    if p_parm == 3:
         f = codecs.open("templates/template_est_rev_byproj.html", 'r')
 
     return f.read()
 
 
 def main():
-    receivers = ['suchat_s@apthai.com', 'pimonwan@apthai.com', 'pornnapa@apthai.com']
-    # receivers = ['suchat_s@apthai.com']
+    # receivers = ['suchat_s@apthai.com', 'pimonwan@apthai.com', 'pornnapa@apthai.com']
+    receivers = ['suchat_s@apthai.com']
     subject = EST_MAIL_SUBJECT
-    # bodyMsg = generateHTML()
+    # bodyMsg = generateHTMLbyProj()
     # bodyMsg = EST_MAIL_BODY
-    bodyMsg = "{0} <br /> {1}".format(readHTMLFile(1), readHTMLFile(2))
+    bodyMsg = "{0} <br /> {1} <br /> {2}".format(generateHTMLDetl(), generateHTMLTotal(), generateHTMLbyProj())
     sender = MAIL_SENDER
 
     attachedFile = []
